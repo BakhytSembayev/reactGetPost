d59797bfa07f071d1156a6b87ccd50bc7130eef1
Bakhyt Sembayev <bahytsembaev@gmail.com>
2024-04-19 08:35:38 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import styles from './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+    const [products, setProducts] = useState([]);
+    const [refreshProducts, setRefreshProducts] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isCreating, setIsCreating] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        fetch('http://localhost:3001/products')
+            .then((loadedData) => loadedData.json())
+            .then((loadedProducts) => {
+                setProducts(loadedProducts);
+            })
+            .finally(() => setIsLoading(false));
+    }, [refreshProducts]);
+
+    const requestAddVacuumCleaner = () => {
+        setIsCreating(true);
+
+        fetch('http://localhost:3001/products', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json;charset=utf-8' },
+            body: JSON.stringify({
+                name: 'Пылесос',
+                price: 4690,
+            }),
+        })
+            .then((rawResponse) => rawResponse.json())
+            .then((response) => {
+                console.log('Пылесос добавлен, ответ сервера:', response);
+                setRefreshProducts(!refreshProducts);
+            })
+            .finally(() => setIsCreating(false));
+    };
+
+    return (
+        <div className={styles.app}>
+            {isLoading
+                ? <div className='loader'></div>
+                : products.map(({ id, name, price }) => (
+                    <div key={id}>
+                        {name} - {price} руб
+                    </div>
+                ))
+            }
+            <button
+	            disabled={isCreating}
+	            onClick={requestAddVacuumCleaner}
+	        >Добавить пылесос</button>
+        </div>
+    );
+};
 
 export default App;
