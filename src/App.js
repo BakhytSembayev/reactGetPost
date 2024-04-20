762c1643931f5c49197aeaaacd7a4ae08dd95550
Bakhyt Sembayev <bahytsembaev@gmail.com>
2024-04-20 10:02:47 +0500

    new branch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ export const App = () => {
     const [refreshProducts, setRefreshProducts] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const [isCreating, setIsCreating] = useState(false);
+    const [isUpdating, setIsUpdating] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
 
     useEffect(() => {
         setIsLoading(true);
@@ -25,16 +27,47 @@ export const App = () => {
             method: 'POST',
             headers: { 'Content-Type': 'application/json;charset=utf-8' },
             body: JSON.stringify({
-                name: 'Пылесос',
-                price: 4690,
+                name: 'VacuumCleaner',
+                price: 300,
             }),
         })
             .then((rawResponse) => rawResponse.json())
             .then((response) => {
-                console.log('Пылесос добавлен, ответ сервера:', response);
+                console.log('playstation добавлен, ответ сервера:', response);
                 setRefreshProducts(!refreshProducts);
             })
             .finally(() => setIsCreating(false));
+    };
+    const requestUpdateSmartphone = () => {
+        setIsUpdating(true);
+
+        fetch('http://localhost:3001/products/002', {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json;charset=utf-8' },
+            body: JSON.stringify({
+                name: 'Смартфон',
+                price: 30900,
+            }),
+        })
+            .then((rawResponse) => rawResponse.json())
+            .then((response) => {
+                console.log('Смартфон обновлён, ответ сервера:', response);
+                setRefreshProducts(!refreshProducts);
+            })
+            .finally(() => setIsUpdating(false));
+    };
+
+    const requestDeleteHairDryer = () => {
+        setIsDeleting(true);
+        fetch('http://localhost:3001/products/b919', {
+            method: 'DELETE',
+        })
+            .then((rawResponse) => rawResponse.json())
+            .then((response) => {
+                console.log('Фен удалён, ответ сервера: ', response);
+                setRefreshProducts(!refreshProducts);
+            })
+            .finally(() => setIsDeleting(false));
     };
 
     return (
@@ -50,7 +83,14 @@ export const App = () => {
             <button
 	            disabled={isCreating}
 	            onClick={requestAddVacuumCleaner}
-	        >Добавить пылесос</button>
+	        >Добавить новый</button>
+
+<button disabled={isUpdating} onClick={requestUpdateSmartphone}>
+                Обновить смартфон
+            </button>
+            <button disabled={isDeleting} onClick={requestDeleteHairDryer}>
+                Удалить фен
+            </button>
         </div>
     );
 };
